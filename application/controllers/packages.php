bc287f2e244346e4ec3dd7a9d205239a25b86474
Faizan Ayubi <indianayubi@gmail.com>
2015-09-26 13:33:44 +0530

    Basic Method added

---
+++ application/controllers/packages.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Example Controller to Acess Files
+ *
+ * @author Faizan Ayubi
+ */
+use Framework\Controller as Controller;
+
+class Packages extends Controller {
+	public function MassageTherapies() {
+		
+	}
+
+	public function BeautyTreatment() {
+		
+	}
+
+	public function CombinationalTherapies()
+	{
+		# code...
+	}
+
+	public function SeasonalTreatment()
+	{
+		# code...
+	}
+
+	public function special()
+	{
+		# code...
+	}
+
+}

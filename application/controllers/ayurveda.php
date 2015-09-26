57c88276d38742247faec7509fd2e1b9b740bfe9
Faizan Ayubi <indianayubi@gmail.com>
2015-09-26 13:33:06 +0530

    Basic Method added

---
+++ application/controllers/ayurveda.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Example Controller to Acess Files
+ *
+ * @author Faizan Ayubi
+ */
+use Framework\Controller as Controller;
+
+class Ayurveda extends Controller {
+	public function index() {
+		
+	}
+
+	public function panchkarma() {
+		
+	}
+
+}

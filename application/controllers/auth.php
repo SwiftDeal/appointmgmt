22a31108a7a9a25a2a7a55b67d37056953d83f42
Faizan Ayubi <indianayubi@gmail.com>
2015-09-28 04:41:27 +0530

    Added Login & Reg

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -15,6 +15,52 @@ class Auth extends Controller {
         $this->defaultExtension = "json";
     }
 
+    public function register() {
+        $this->defaultLayout = "layouts/blank";
+        $this->setLayout();
+        $this->seo(array("title" => "Register", "view" => $this->getLayoutView()));
+        $view = $this->getActionView();
+
+        if (RequestMethods::post("action") == "register") {
+            $exist = User::first(array("email = ?" => RequestMethods::post("email")));
+            if (!$exist) {
+                $user = new User(array(
+                    "name" => RequestMethods::post("name"),
+                    "email" => RequestMethods::post("email"),
+                    "password" => sha1(RequestMethods::post("password")),
+                    "phone" => RequestMethods::post("phone"),
+                    "admin" => FALSE,
+                    "gender" => "male"
+                ));
+                $user->save();
+                $view->set("message", "Your account has been created contact HR to activate");
+            } else {
+                $view->set("message", 'Account exists, login from <a href="/admin/login">here</a>');
+            }
+        }
+    }
+
+    public function login() {
+        $this->defaultLayout = "layouts/blank";
+        $this->setLayout();
+        $this->seo(array("title" => "Login", "view" => $this->getLayoutView()));
+        $view = $this->getActionView();
+
+        if (RequestMethods::post("action") == "login") {
+            $user = User::first(array(
+                "email = ?" => RequestMethods::post("email"),
+                "password = ?" => sha1(RequestMethods::post("password")),
+                "live" => TRUE
+            ));
+            if ($user) {
+                $this->setUser($user);
+                self::redirect("/admin");
+            } else {
+                $view->set("message", "User not exist or blocked");
+            }
+        }
+    }
+
     public function logout() {
         $this->setUser(false);
         self::redirect("/login");

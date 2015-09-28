e95c51f2ea3ae2c9419e5e1ad7defef4d30fe62f
Faizan Ayubi <indianayubi@gmail.com>
2015-09-28 14:07:04 +0530

    Package Mange added

--- application/controllers/packages.php
+++ application/controllers/packages.php
@@ -6,6 +6,7 @@
  * @author Faizan Ayubi
  */
 use Framework\Controller as Controller;
+use Framework\RequestMethods as RequestMethods;
 
 class Packages extends Admin {
 
@@ -13,13 +14,21 @@ class Packages extends Admin {
      * @before _secure, changeLayout
      */
 	public function create() {
-		$this->seo(array("title" => "Create Package", "keywords" => "admin", "description" => "admin", "view" => $this->getLayoutView()));
+		$this->seo(array("title" => "Create Package", "view" => $this->getLayoutView()));
 		$view = $this->getActionView();
 	}
 
-	public function view($id='') {
+	public function details($title, $id='') {
 		$item = Item::first(array("id = ?" => $id));
-		$this->seo(array("title" => $item->title, "keywords" => "admin", "description" => "admin", "view" => $this->getLayoutView()));
+		$this->seo(array(
+			"title" => $item->title,
+			"keywords" => $item->title,
+			"description" => substr(strip_tags($item->details), 0, 150),
+			"view" => $this->getLayoutView()
+		));
+		$view = $this->getActionView();
+
+		$view->set("package", $item);
 	}
 
 	/**
@@ -28,6 +37,20 @@ class Packages extends Admin {
     public function edit($id='') {
     	$this->seo(array("title" => "Edit Package", "keywords" => "admin", "description" => "admin", "view" => $this->getLayoutView()));
     }
+
+    /**
+     * @before _secure, changeLayout
+     */
+	public function manage() {
+		$this->seo(array("title" => "Manage Package", "view" => $this->getLayoutView()));
+		$view = $this->getActionView();
+
+		$page = RequestMethods::get("page", 1);
+		$packages = Item::all(array(), array("*"), "id", "desc", "10", $page);
+
+		$view->set("page", $page);
+		$view->set("packages", $packages);
+	}
 	
 	public function MassageTherapies() {
 		

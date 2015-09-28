9b2735a661d76ba4d9e1ed5179d7f880ffc13942
Faizan Ayubi <indianayubi@gmail.com>
2015-09-28 13:50:09 +0530

    Added Manage Controller

--- application/controllers/packages.php
+++ application/controllers/packages.php
@@ -13,18 +13,20 @@ class Packages extends Admin {
      * @before _secure, changeLayout
      */
 	public function create() {
-		
+		$this->seo(array("title" => "Create Package", "keywords" => "admin", "description" => "admin", "view" => $this->getLayoutView()));
+		$view = $this->getActionView();
 	}
 
 	public function view($id='') {
-		
+		$item = Item::first(array("id = ?" => $id));
+		$this->seo(array("title" => $item->title, "keywords" => "admin", "description" => "admin", "view" => $this->getLayoutView()));
 	}
 
 	/**
      * @before _secure, changeLayout
      */
     public function edit($id='') {
-    	
+    	$this->seo(array("title" => "Edit Package", "keywords" => "admin", "description" => "admin", "view" => $this->getLayoutView()));
     }
 	
 	public function MassageTherapies() {
